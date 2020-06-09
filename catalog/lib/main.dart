457d75714605a82c88dfeca417c86a64019e4ef5
hackycy <qa894178522@qq.com>
2020-06-09 13:38:13 +0800

    examples catalog

---
+++ catalog/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Instead run:\nflutter run lib/xxx.dart',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
