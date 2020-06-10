b2cdce4dfc637b32d5fa1f951bcee30c1fdf1e7e
hackycy <qa894178522@qq.com>
2020-06-10 12:04:23 +0800

    practice listview widget

--- catalog/lib/listview.dart
+++ catalog/lib/listview.dart
@@ -23,18 +23,42 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
+      padding: EdgeInsets.all(2),
       children: <Widget>[
 
         ListTile(
-          title: Text('Text'),
+          leading: Icon(
+            Icons.home,
+            size: 40,
+            color: Colors.yellow,
+          ),
+          title: Text(
+            '英雄联盟',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text('UZI退役了'),
         ),
 
         ListTile(
-          title: Text('Text'),
+          leading: Image.network('https://images.pexels.com/photos/4561540/pexels-photo-4561540.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+          title: Text('CFPL'),
+          subtitle: Text('我不知道发生了什么'),
         ),
 
         ListTile(
-          title: Text('Text'),
+          title: Text('QQ飞车'),
+          subtitle: Text('S多少赛季开始了'),
+          trailing: Icon(Icons.settings),
+        ),
+
+        ListTile(
+          leading: Image.network('https://images.pexels.com/photos/4561540/pexels-photo-4561540.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+          title: Text('CFPL'),
+          subtitle: Text('我不知道发生了什么'),
+          trailing: Image.network('https://images.pexels.com/photos/4262185/pexels-photo-4262185.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
         ),
 
       ],
