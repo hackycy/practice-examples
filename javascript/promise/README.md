10621cdbd1b1a59292e2897e086aecbe1d7e2e17
hackycy <qa894178522@qq.com>
2020-11-18 12:05:30 +0800

    promise

--- javascript/promise/README.md
+++ javascript/promise/README.md
@@ -1,3 +1,5 @@
 手写Promise案例
 
-https://www.bilibili.com/video/BV1oE411677i?p=21
+es5写法
+
+https://www.bilibili.com/video/BV1oE411677i?p=26
--- javascript/promise/promise.js
+++ javascript/promise/promise.js
@@ -6,7 +6,51 @@
      * @param {Function} executor 
      */
     function Promise(executor) {
-      console.log('custom promise')
+
+      // promise状态
+      this.status = 'pending'
+      // 存储数据结果
+      this.data = undefined
+      //每个元素的结构为 { onResolved(), onRejected() }
+      this.callbacks = []
+      
+      function resolve(value) {
+        if (this.status !== 'pending') {
+          // 如果当前状态不为pending，则无法再次进行改变状态
+          return
+        }
+        this.status = 'resolved'
+        this.data = value
+        if (this.callbacks.length > 0) {
+          setTimeout(() => {
+            this.callbacks.forEach(cb => {
+              cb.onResolved(value)
+            });
+          })
+        }
+      }
+
+      function reject(reason) {
+        if (this.status !== 'pending') {
+          return
+        }
+        this.status = 'rejected'
+        this.data = reason
+        if (this.callbacks.length > 0) {
+          setTimeout(() => {
+            this.callbacks.forEach(cb => {
+              cb.onRejected(reason)
+            })
+          })
+        }
+      }
+
+      // 调用执行器
+      try {
+        executor(resolve.bind(this), reject.bind(this))
+      } catch (e) {
+        reject(e)
+      }
     }
 
     /**
@@ -15,7 +59,8 @@
      * @param {Function} onRejected 
      */
     Promise.prototype.then = function(onResolved, onRejected) {
-
+      // 假设当前状态为pending，则保存毁掉函数
+      this.callbacks.push({ onResolved, onRejected })
     }
 
     window.Promise = Promise
