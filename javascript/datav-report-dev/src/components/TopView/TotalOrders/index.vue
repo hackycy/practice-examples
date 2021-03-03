f1870d21c62b5d405f340df283a21f2428896319
hackycy <qa894178522@qq.com>
2021-03-03 13:12:08 +0800

    累计订单量

--- javascript/datav-report-dev/src/components/TopView/TotalOrders/index.vue
+++ javascript/datav-report-dev/src/components/TopView/TotalOrders/index.vue
@@ -2,7 +2,7 @@
   <div>
     <common-card title="累计订单量" value="3,018,247">
       <template>
-        <div id="total-orders-chart" style="height=100%;width=100%;"></div>
+        <div id="total-orders-chart" :style="{ width: '100%', height: '100%' }"></div>
       </template>
       <template slot="footer">
         <div class="total">昨日订单量 <span class="emphasis">2,000,000</span></div>
@@ -17,8 +17,39 @@ import commonCardMixin from '@/mixins/commonCardMixin'
 export default {
   name: 'TotalOrders',
   mixins: [commonCardMixin],
-  mounted: () => {
-    console.log('mounted')
+  mounted: function () {
+    const chatDom = document.getElementById('total-orders-chart')
+    const chart = this.$echarts.init(chatDom)
+    chart.setOption({
+      xAxis: {
+        type: 'category',
+        show: false,
+        boundaryGap: false
+      },
+      yAxis: {
+        show: false
+      },
+      series: [{
+        type: 'line',
+        data: [620, 432, 220, 534, 790, 430, 220, 320, 532, 834, 690, 530, 220, 620],
+        itemStyle: {
+          opacity: 0
+        },
+        lineStyle: {
+          width: 0
+        },
+        areaStyle: {
+          color: 'purple'
+        },
+        smooth: true
+      }],
+      grid: {
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
+      }
+    })
   }
 }
 </script>
