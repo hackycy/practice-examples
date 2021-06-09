2e281a53e6285bfc64deb6d4b8346fd376360d82
hackycy <qa894178522@qq.com>
2021-06-09 18:22:26 +0800

    tmp：promise define

--- javascript/promise/README.md
+++ javascript/promise/README.md
@@ -1,5 +1,3 @@
 手写Promise案例
 
 es5写法
-
-https://www.bilibili.com/video/BV1oE411677i?p=26
--- javascript/promise/promise.js
+++ javascript/promise/promise.js
@@ -1,68 +1,104 @@
-(
-  function (window) {
+/**
+ * 手写promise
+ * https://www.ituring.com.cn/article/66566
+ * https://promisesaplus.com/
+ */
+(function (window) {
 
-    /**
-     * Promise Construtor
-     * @param {Function} executor 
-     */
-    function Promise(executor) {
+  /**
+   * 检测是否是一个函数
+   * @param {any} func 
+   */
+  function isFunction(func) {
+    return typeof func === 'function' || Object.toString.call(func) === '[object Function]'
+  }
 
-      // promise状态
-      this.status = 'pending'
-      // 存储数据结果
-      this.data = undefined
-      //每个元素的结构为 { onResolved(), onRejected() }
-      this.callbacks = []
-      
-      function resolve(value) {
-        if (this.status !== 'pending') {
-          // 如果当前状态不为pending，则无法再次进行改变状态
-          return
-        }
-        this.status = 'resolved'
-        this.data = value
-        if (this.callbacks.length > 0) {
-          setTimeout(() => {
-            this.callbacks.forEach(cb => {
-              cb.onResolved(value)
-            });
-          })
-        }
-      }
+  // Promise 状态
+  const State = {
+    // 处于等待态时，promise 需满足以下条件： 可以迁移至执行态或拒绝态
+    PENDING: Symbol.for('pending'),
 
-      function reject(reason) {
-        if (this.status !== 'pending') {
-          return
-        }
-        this.status = 'rejected'
-        this.data = reason
-        if (this.callbacks.length > 0) {
-          setTimeout(() => {
-            this.callbacks.forEach(cb => {
-              cb.onRejected(reason)
-            })
-          })
-        }
-      }
+    // 处于执行态时，promise 需满足以下条件： 不能迁移至其他任何状态 | 必须拥有一个不可变的终值
+    FULFILLED: Symbol.for('fulfilled'),
 
-      // 调用执行器
+    // 处于拒绝态时，promise 需满足以下条件：不能迁移至其他任何状态 | 必须拥有一个不可变的据因
+    REJECTED: Symbol.for('rejected')
+  }
+
+  function changeState(state) {
+    if (this.state === state) {
+      throw new Error('不能改变至相同的状态')
+    }
+    if (!this.isPending()) {
+      throw new Error('处理执行态或者拒绝态时不可迁移至其它的状态')
+    }
+    if (state === State.PENDING) {
+      throw new Error('只允许迁移至迁移至执行态或拒绝态')
+    }
+    this.state = state
+  }
+
+  function resolve(promise, x) {
+    if (promise === x) {
+
+    }
+  }
+
+  function reject(e) {
+
+  }
+
+  function Promise(executor) {
+    this.state = State.PENDING
+
+    // 任何合法的 JavaScript 值（包括undefined、thenable 或 promise）
+    this.value = undefined
+
+    // 被拒绝的原因
+    this.reason = undefined
+
+    // { fulfill, reject }
+    this.queue = []
+
+    // 必须是一个函数
+    if (isFunction(executor)) {
       try {
         executor(resolve.bind(this), reject.bind(this))
       } catch (e) {
-        reject(e)
+        reject.call(this, e)
       }
     }
-
-    /**
-     * then
-     * @param {Function} onResolved 
-     * @param {Function} onRejected 
-     */
-    Promise.prototype.then = function(onResolved, onRejected) {
-      // 假设当前状态为pending，则保存毁掉函数
-      this.callbacks.push({ onResolved, onRejected })
-    }
-
-    window.Promise = Promise
   }
-)(window)
+
+  Promise.prototype.then = function (onFulfilled, onRejected) {
+    const _self = this
+    // 如果onFulfilled 或者 onRejected 不是一个函数，则忽略
+    const onfulfilled = isFunction(onFulfilled) ? onFulfilled : function (v) { return v }
+    const onrejected = isFunction(onRejected) ? onRejected : function (e) { throw e }
+
+    // then 方法必须返回一个 promise 对象
+    const p = new Promise(function(resolve, reject) {
+
+    })
+    return p
+  }
+
+  /**
+   * 判断函数状态
+   */
+  Promise.prototype.isPending = function () {
+    return this.state === State.PENDING
+  }
+
+  Promise.prototype.isFulfilled = function () {
+    return this.state === State.FULFILLED
+  }
+
+  Promise.prototype.isRejected = function () {
+    return this.state === State.REJECTED
+  }
+
+  // register
+  window.Promise = Promise
+
+})(window)
