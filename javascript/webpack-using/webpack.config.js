cff521707e3dfb570efb09862cdcbd47ef9a4e3d
hackycy <qa894178522@qq.com>
2021-06-21 18:05:54 +0800

    cache babel loader & include

--- javascript/webpack-using/webpack.config.js
+++ javascript/webpack-using/webpack.config.js
@@ -21,13 +21,18 @@ module.exports = {
     alias: {
       '@': resolve('src')
     },
+    // 在导入语句没带文件后缀时，Webpack 会自动带上后缀后去尝试访问文件是否存在。 且优先命中ts
     extensions: ['.ts', '.js']
   },
+  // 处理模块
   module: {
+    // 配置loader
     rules: [
       {
         test: /\.ts$/,
-        use: ['babel-loader'],
+        use: ['babel-loader?cacheDirectory'],
+        // 只命中src目录里的ts文件
+        include: resolve('src')
       },
     ]
   },
