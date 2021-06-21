c17defa2a63cd6b08ff63d7de307dd4195f17ab6
hackycy <qa894178522@qq.com>
2021-06-21 18:40:57 +0800

    add ESLintPlugin

--- javascript/webpack-using/webpack.config.js
+++ javascript/webpack-using/webpack.config.js
@@ -1,6 +1,7 @@
 'use strict'
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const ENV = process.env.NODE_ENV ?? 'development'
@@ -38,6 +39,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new ESLintPlugin({
+      extensions: ['ts', 'js']
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       chunks: ['app'],
